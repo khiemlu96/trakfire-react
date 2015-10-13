7fd6a2e62866f7bd717752b9019f2412085af00f
Grant Collins <grantcol@usc.edu>
2015-10-12 22:05:46 -0700

    cleanup script

--- cleanPosts.rb
+++ cleanPosts.rb
@@ -1,9 +1,13 @@
 p = Post.all
 p.each do |pp|
-  if !pp.stream_url
-  	puts 'DELETING'
-    pp.destroy
+  #pp.date = Date.today.to_datetime.strftime("%m/%d/%Y")
+  if pp.song_id
+	  song = Song.find(pp.song_id)
+	  pp.artist = song.artist
+	  pp.waveform_url = song.waveform_url
+	  pp.img_url = song.thumb_url
+	  pp.title = song.title
+	  pp.stream_url = song.stream_url
+	  pp.save
   end
-  pp.date = Date.today.to_datetime.strftime("%m/%d/%Y")
-  pp.save
 end
