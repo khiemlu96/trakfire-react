a665481e1791ec154cc3e6da5d056c4df99b7ba4
Grant Collins <grantcol@usc.edu>
2015-11-05 05:47:58 -0800

    fix the user data acq bug

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -15,18 +15,21 @@ class TokensController < ApplicationController
       user_data = access_token.request(:get, "https://api.twitter.com/1.1/account/verify_credentials.json")
       j_user = JSON.parse(user_data.body)
       logger.info "USER DATA"
-      logger.info j_user
+      logger.info j_user['name']
+      logger.info j_user['description']
+      logger.info j_user['location']
+      logger.info j_user['profile_image_url_https']
       user = User.find_or_create_by(uid: access_token.params[:user_id]) do |u| 
         u.handle = access_token.params[:screen_name] 
-        u.username = j_user[:name]
-        u.img = j_user[:img]
-        u.tbio = j_user[:description]
-        u.location = j_user[:location]
+        u.username = j_user['name']
+        u.img = j_user['profile_image_url_https']
+        u.tbio = j_user['description']
+        u.location = j_user['location']
         new_user = true
       end
       jwt = JWT.encode({uid: user.uid, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base)
       if new_user
-        redirect_to ENV['ORIGIN'] + "/email?jwt=#{jwt}"
+        redirect_to ENV['ORIGIN'] + "/email/?jwt=#{jwt}"
       else 
         redirect_to ENV['ORIGIN'] + "?jwt=#{jwt}"
       end
