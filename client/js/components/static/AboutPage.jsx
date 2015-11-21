d3a2f818d103486c1fe848130ee5f3d342958404
Grant Collins <grantcol@usc.edu>
2015-11-21 11:49:37 -0800

    new about

--- client/js/components/static/AboutPage.jsx
+++ client/js/components/static/AboutPage.jsx
@@ -9,7 +9,9 @@ var AboutPage = React.createClass({
     return (
       <div id="about-page">
         <h1>About</h1>
-        <p>We are on a mission to level the playing field in the music industry. Through crowd curation we are democratizing discovery so that regardless of money, marketing or networks, good music surfaces up.</p>
+        <p>We’re on a mission to level the playing field in the music industry. For every artist on the radio, there are thousands of talented independent artists whose music goes unheard. We believe that musicians should be discovered for their artistry, not just because of big marketing budgets or exclusive industry connections. This is why we built Trakfire.</p>
+
+        <p>Our vision is to create a strong community comprised of both music creators and fans that enjoy sharing and discovering great new music. Together, let’s help these amazing artists get the exposure they deserve and listen to some good music while we’re at it.</p>
       </div>
     );
   },
