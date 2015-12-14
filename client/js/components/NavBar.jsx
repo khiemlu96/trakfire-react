374fcd1415bd57d8945cb8a9dea01c980ddce4a9
Grant Collins <grantcol@usc.edu>
2015-12-14 11:21:07 -0800

    ungrey navbar

--- client/js/components/NavBar.jsx
+++ client/js/components/NavBar.jsx
@@ -47,12 +47,12 @@ var NavBar = React.createClass({
     if(this.props.isLoggedIn) {
       var signinLink = <a href="#!" onClick={this.handleSignOut}> SIGN OUT </a>
       var profileLink = <Link to={'/profile/'+this.props.user.id} className="tf-uppercase">{this.props.user.handle}</Link>;
-      if(this.props.isAdmin || this.props.user.canPost) { 
-        var postLink = <Link to='/post'>POST</Link> 
-      } else {
+      //if(this.props.isAdmin || this.props.user.canPost) { 
+      var postLink = <Link to='/post'>POST</Link> 
+      /*} else {
         var tooltip = <Tooltip>Posting is invite only</Tooltip>;
         var postLink = <OverlayTrigger placement="left" overlay={tooltip}><a className="tf-inactive">POST</a></OverlayTrigger>;//<a href="" className="tf-inactive">POST</a>;
-      }
+      }*/
     } else {
       var signinLink = <a href={this.props.origin+'/request_token'}> SIGN IN </a>
       var profileLink = "";
