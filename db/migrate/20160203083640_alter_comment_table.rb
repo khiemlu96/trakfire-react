d4cd14d2e35a698c3a5fa50a622344c9d0d525f7
Grant Collins <grantcol@usc.edu>
2016-02-08 15:57:07 -0800

    add migration

--- db/migrate/20160203083640_alter_comment_table.rb
+++ db/migrate/20160203083640_alter_comment_table.rb
@@ -1,6 +1,7 @@
 class AlterCommentTable < ActiveRecord::Migration
   def change
   	add_column :comments, :parent_id, :integer
+  	add_column :comments, :post_id, :integer
   	add_column :comments, :user_id, :integer
   	add_column :comments, :comment_detail, :string
   end
