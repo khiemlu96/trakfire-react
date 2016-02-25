16abed16c624b7e235a5d7c958a29bd6fbca76fd
hrishi-thombare <hrishikesh.thombare@syntrosys.com>
2016-02-25 20:19:51 +0530

    TF-43

--- client/js/components/TrakfirePlayerInfo.jsx
+++ client/js/components/TrakfirePlayerInfo.jsx
@@ -18,13 +18,20 @@ var TrakfirePlayerInfo = React.createClass({
    */
   render: function() {
    // console.log('RENDER TRACK INFO', this.props );
+    var songTitle = this.props.title;
+    if(songTitle.length > 35)
+    {
+      songTitle = songTitle.substring(0,32);
+      songTitle = songTitle + "...";
+    }
+    
     return (
       <div className="tf-player-info">
         
           <Link to={'/post/'+ this.props.post.id}><img className="tf-player-info-img" src={this.props.img}></img></Link>
           <div className="tf-player-info-text">
             <div>
-              <Link to={'/post/'+ this.props.post.id}><p className="tf-player-title-info">{this.props.title}</p></Link>
+              <Link to={'/post/'+ this.props.post.id}><p className="tf-player-title-info">{songTitle}</p></Link>
               <Link to={'/post/'+ this.props.post.id}><p className="tf-player-artist-info">{this.props.artist}</p></Link>
             </div>
           </div>        
