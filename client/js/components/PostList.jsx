d20b3cc98e0ee66b663f66ee5c326fcc0a731d00
Grant Collins <grantcol@usc.edu>
2016-04-04 12:00:49 -0700

    add alt text pre placeholder img

--- client/js/components/PostList.jsx
+++ client/js/components/PostList.jsx
@@ -415,21 +415,21 @@ var PostsList = React.createClass({
       carousalItemHtml = 
           <Carousel>
             <CarouselItem>
-              <img width={900} height={500} alt="900x500" src={original_images[keys[0]].file} />
+              <img width={900} height={500} alt="900x500" src={original_images[keys[0]] ? original_images[keys[0]].file : ""} alt="pure fire pic"/>
               <div className="carousel-caption">
                 <h3>First slide label</h3>
                 <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
               </div>
             </CarouselItem>
             <CarouselItem>
-              <img width={900} height={500} alt="900x500" src={original_images[keys[1]].file} />
+              <img width={900} height={500} alt="900x500" src={original_images[keys[1]] ? original_images[keys[1]].file : ""} alt="pure fire pic"/>
               <div className="carousel-caption">
                 <h3>Second slide label</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
             </CarouselItem>
             <CarouselItem>
-              <img width={900} height={500} alt="900x500" src={original_images[keys[2]].file} />
+              <img width={900} height={500} alt="900x500" src={original_images[keys[2]] ? original_images[keys[2]].file : ""} alt="pure fire pic"/>
               <div className="carousel-caption">
                 <h3>Third slide label</h3>
                 <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
