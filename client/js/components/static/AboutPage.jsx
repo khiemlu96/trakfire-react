4762e6de64539b34cfdbc03b8817176e5812ec26
Arjun Mehta <arjunmehta2014@gmail.com>
2016-04-06 03:30:50 -0700

    updated static page styles

--- client/js/components/static/AboutPage.jsx
+++ client/js/components/static/AboutPage.jsx
@@ -7,11 +7,9 @@ var AboutPage = React.createClass({
    */
   render: function() {
     return (
-      <div id="about-page">
-        <h1>About</h1>
-        <p>We’re on a mission to level the playing field in the music industry. For every artist on the radio, there are thousands of talented independent artists whose music goes unheard. We believe that musicians should be discovered for their artistry, not just because of big marketing budgets or exclusive industry connections. This is why we built Trakfire.</p>
-
-        <p>Our vision is to create a strong community comprised of both music creators and fans that enjoy sharing and discovering great new music. Together, let’s help these amazing artists get the exposure they deserve and listen to some good music while we’re at it.</p>
+      <div className="container p-t-md about-page">
+        <span className="tf-static-page-header">ABOUT</span>
+        <p>Trakfire is an invite-only community of people who have the feel for music. It’s a place for tastemakers, both emerging and well known, to share their best finds from the vast seas of Soundcloud and YouTube. There's more noise out there than ever before, and worthy artistry often loses out to money, marketing, and connections. Our hope for Trakfire is to create a space where great music can surface up democratically, powered by people who love and know music.</p>
       </div>
     );
   },
