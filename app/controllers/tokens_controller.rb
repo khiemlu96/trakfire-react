5a4cec642380ba9c12be6575fae7bfd528924a81
Grant Collins <grantcol@usc.edu>
2016-04-05 19:53:59 -0700

    fix request

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -12,7 +12,7 @@ class TokensController < ApplicationController
     if oauth.present?
       request_token = OAuth::RequestToken.new(TWITTER, oauth.token, oauth.secret)
       access_token = request_token.get_access_token(oauth_verifier: params[:oauth_verifier])
-      user_data = access_token.request(:get, "https://api.twitter.com/1.1/account/verify_credentials?include_email=true.json")
+      user_data = access_token.request(:get, "https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true")
       j_user = JSON.parse(user_data.body)
       user = User.find_or_create_by(uid: access_token.params[:user_id]) do |u| 
         u.handle = access_token.params[:screen_name] 
