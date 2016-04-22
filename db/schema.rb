968f44471ed418af8060d409c2bd24b3624a8663
Grant Collins <grantcol@usc.edu>
2016-04-22 13:24:43 -0700

    merge notifs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418070830) do
+ActiveRecord::Schema.define(version: 20160419100432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20160418070830) do
     t.string   "statement"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "sc1"
+    t.string   "sc2"
+    t.string   "sc3"
   end
 
   create_table "comments", force: :cascade do |t|
