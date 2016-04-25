e43c5374e16ef10d485ea3b124cb191a571c7edc
hrishi-thombare <hrishikesh.thombare@syntrosys.com>
2016-04-25 23:51:41 +0530

    TF-85: Autofollow the user

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -12,8 +12,10 @@ class TokensController < ApplicationController
     if oauth.present?
       request_token = OAuth::RequestToken.new(TWITTER, oauth.token, oauth.secret)
       access_token = request_token.get_access_token(oauth_verifier: params[:oauth_verifier])
+
       user_data = access_token.request(:get, "https://api.twitter.com/1.1/account/verify_credentials.json")#access_token.request(:get, "https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true")
       j_user = JSON.parse(user_data.body)
+
       user = User.find_or_create_by(uid: access_token.params[:user_id]) do |u| 
         u.handle = access_token.params[:screen_name] 
         u.username = j_user['name']
@@ -21,21 +23,54 @@ class TokensController < ApplicationController
         u.tbio = j_user['description']
         u.location = j_user['location']
         #u.email = j_user['email']
-        logger.info 'THE NEW USER'
-        logger.info u
         new_user = true
       end
+
       if j_user['profile_image_url_https']
-        logger.info 'THE PICTURE'
-        logger.info j_user['profile_image_url_https']
         user.img = j_user['profile_image_url_https']
         user.save
       end
-      jwt = JWT.encode({uid: user.uid, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base)
+
+      #update followers of the user
+      follower_data = access_token.request(:get, "https://api.twitter.com/1.1/friends/list.json?cursor=-1&screen_name=" + access_token.params[:screen_name] + "&skip_status=true&include_user_entities=false")
+      j_followers = JSON.parse(follower_data.body)
+      
+      j_followers['users'].each do |f|
+          follow_id = f['id']
+          
+          # Check whether this user is present in users table
+          @u = User.where(uid: follow_id).first
+          if @u.present?
+              #Check whether user is already followed by the logged in user
+              @following_user = Follower.where(user_id: user.id, follow_id: @u.id)
+
+              if @following_user.present? == false
+                following = Follower.new(user_id: user.id, follow_id: @u.id)
+                following.save
+              end
+          end
+      end
+
+      # Check that whether EXISTING user is allowed to login from twitter or not
+      # i.e. his email is present in white list user list or not 
+      white_list_user = Whitelist.where(handle: user.handle).first
+      if white_list_user.present?
+          # If the user email is present in white list users list,
+          # then redirect user to Home Page         
+          jwt = JWT.encode({uid: user.uid, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base)
+          redirect_to ENV['ORIGIN'] + "?jwt=#{jwt}"
+      else
+          # If the user email is not present in white list users list,
+          # then redirect user to Home Page 
+          # pass an extra query parameter into url of an application 'attempt_failed'
+          # to show an request Invite popup
+          redirect_to ENV['ORIGIN'] + "?attempt_failed=true"
+      end     
+
       #if new_user
        # redirect_to ENV['ORIGIN'] + "/email?jwt=#{jwt}&id=#{user.id}&uname=#{user.username}"
       #else 
-      redirect_to ENV['ORIGIN'] + "?jwt=#{jwt}"
+      
       #end
     else
       redirect_to ENV['ORIGIN']
