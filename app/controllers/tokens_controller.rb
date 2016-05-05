ba9a165e17165dffbe6ad955051d02b559c6e54d
hrishi-thombare <hrishikesh.thombare@syntrosys.com>
2016-05-05 19:57:17 +0530

    Tempaorily disabling whitelist check

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -61,33 +61,36 @@ class TokensController < ApplicationController
           end
       end
 
+      #Temporily disabling the whitelist check 
+
       # Check that whether user is allowed to login from twitter or not
       # i.e. his email is present in white list user list or not 
-      if user.isAdmin != true
-        white_list_user = Whitelist.where(handle: user.handle).first
-        if white_list_user.present?
+      #if user.isAdmin != true
+        #white_list_user = Whitelist.where(handle: user.handle).first
+        #if white_list_user.present?
             # If the user email is present in white list users list,
             # then redirect user to Home Page         
-            jwt = JWT.encode({uid: user.uid, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base)
-            redirect_to ENV['ORIGIN'] + "?jwt=#{jwt}"
-        else
+            #jwt = JWT.encode({uid: user.uid, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base)
+            #redirect_to ENV['ORIGIN'] + "?jwt=#{jwt}"
+        #else
             # If the user email is not present in white list users list,
             # then redirect user to Home Page 
             # pass an extra query parameter into url of an application 'attempt_failed'
             # to show an request Invite popup
-            redirect_to ENV['ORIGIN'] + "?attempt_failed=true"
-        end
-      else 
+            #redirect_to ENV['ORIGIN'] + "?attempt_failed=true"
+        #end
+      #else 
         # If the user is admin,
         # then redirect user to Home Page
-        jwt = JWT.encode({uid: user.uid, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base)
-        redirect_to ENV['ORIGIN'] + "?jwt=#{jwt}"
-      end          
+        #jwt = JWT.encode({uid: user.uid, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base)
+        #redirect_to ENV['ORIGIN'] + "?jwt=#{jwt}"
+      #end          
 
       #if new_user
        # redirect_to ENV['ORIGIN'] + "/email?jwt=#{jwt}&id=#{user.id}&uname=#{user.username}"
-      #else 
-      
+      #else
+        jwt = JWT.encode({uid: user.uid, exp: 1.day.from_now.to_i}, Rails.application.secrets.secret_key_base) 
+        redirect_to ENV['ORIGIN'] + "?jwt=#{jwt}"
       #end
     else
       redirect_to ENV['ORIGIN']
