6b2f895139551c847074f393d619dae62deed6d3
Grant Collins <grantcol@usc.edu>
2016-05-20 13:43:15 -0700

    new score values + weekly score updates

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -25,8 +25,11 @@ class VotesController < ApplicationController
 			author.score = 0
 		end
 
-	    newScore = author.score += 10
-		author.update( { 'score' => newScore } )
+	    new_score = author.score += 100.0
+	    new_week_score = author.score_weekly += 100.0
+	    
+		author.update( { 'score' => new_score } )
+		author.update( { 'score_weekly' => new_week_score } )
 		author.save 
 
 		logger.info post.vote_count
