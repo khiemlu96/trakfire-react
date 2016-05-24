123adb762683275426c800592e29abbd024215ce
arjun mehta <arjunmehta2014@gmail.com>
2016-05-23 18:03:24 -0700

    updated copy

--- client/js/components/static/FAQ.jsx
+++ client/js/components/static/FAQ.jsx
@@ -8,7 +8,7 @@ var FAQ = React.createClass({
         <span className="tf-static-page-header">Frequently Asked Questions</span>
         <h4>What is Trakfire?</h4>
         <p>
-          Trakfire is an invite-only community of people who have the feel for music. It’s a place for tastemakers, both emerging and well known, to share their best finds from the vast seas of Soundcloud and YouTube. There's more noise out there than ever before, and worthy artistry often doesn't receive as much visibility as those with money, marketing, and connections. Our hope for Trakfire is to create a space where great music can surface up democratically, powered by people who love and know music.
+          Trakfire is a community of people who have the feel for music. It’s a place for tastemakers, both no-names and brand names, to share their best finds from the vast seas of Soundcloud and YouTube. There's more noise out there than ever before, and worthy artistry often doesn't receive as much visibility as those with money, marketing, and connections. Our goal is to 1) create a space where great music can surface democratically, powered by people who love and know music, and 2) move the needle in music culture as a collective mass.
         </p>
         <br />
 
