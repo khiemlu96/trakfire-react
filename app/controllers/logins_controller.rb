cc33c3bdcd9dde74db75b867677379bf9e3f1ff1
Grant Collins <grantcol@usc.edu>
2016-05-26 17:15:13 -0700

    fix syntax error

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -8,14 +8,14 @@ class LoginsController < ApplicationController
 
   	if user_to_auth.password == password
   	  logger.info "USER AUTHENTICATED"
-  	  auth_object = { 'status' : '200' }
+  	  auth_object = { 'status' => '200' }
   	  render json:  auth_object
   	elsif user_to_auth.password == nil
   	  user_to_auth.password = password
-	  auth_object = { 'status' : '200' }
+	  auth_object = { 'status' => '200' }
   	  render json:  auth_object
   	else
-  	  auth_object = { 'status' : '403'}
+  	  auth_object = { 'status' => '403'}
   	end
 
   end
