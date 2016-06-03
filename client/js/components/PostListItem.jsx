a8fb244b89bae66aa7150f4061f6c36c94db89e0
Grant Collins <grantcol@usc.edu>
2016-06-03 14:05:23 -0700

    fix the jobboard and add placeholder

--- client/js/components/PostListItem.jsx
+++ client/js/components/PostListItem.jsx
@@ -461,7 +461,7 @@ var PostListItem = React.createClass({
             <div className = "media-left">
                 <span className = "tf-media-number">{isNumbered ? this.props.number + 1 : ""} </span> 
                 <a href = "#" className = "tf-media-wrap" onClick = {this.playPauseTrack}>
-                    <img className = "media-object tf-media-thumbnail" width = "64" src = {post.img_url} alt = "..."></img> 
+                    <img className = "media-object tf-media-thumbnail" width = "64" src = {post.img_url ? post.img_url : "img/tf-placeholder.png"} alt = "..."></img> 
                     <div className = "tf-media-thumbnail-overlay" ref = "overlaybg">
                         <span className = {paused} ref = "overlay"></span>
                     </div>
