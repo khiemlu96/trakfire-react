e835605eb39b1e2d548bc149fdead0f330bf93f9
arjun mehta <arjunmehta2014@gmail.com>
2016-08-11 12:41:28 -0700

    grammar edit

--- client/js/components/static/AboutPage.jsx
+++ client/js/components/static/AboutPage.jsx
@@ -16,7 +16,7 @@ var AboutPage = React.createClass({
       <div className="container p-t-md about-page">
         <span className="tf-static-page-header">ABOUT</span>
         <p>
-          Trakfire is a discovery platform powered by the people who heard it first. The aux gods and the stream DJs. The ones who wait for the backseat to ask "what is this?". We want the kids who had K Dot on an iPod before m.A.A.d city. The ones who sang with Lizzy Grant pre-Lana Del Rey. We are a collective of tastemakers who knew the artists of 2016 in 2010. And we're gonna find the next wave too.
+          Trakfire is a discovery platform powered by the people who heard it first. The aux gods and the stream DJs. The ones who wait for the backseat to ask "what is this?" We want the kids who had K Dot on an iPod before m.A.A.d city. The ones who sang with Lizzy Grant pre-Lana Del Rey. We are a collective of tastemakers who knew the artists of 2016 in 2010. And we're gonna find the next wave too.
         </p>
 
         <br/>
